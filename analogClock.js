48f674f4c63db303535c4504a2a9933c71eef3f5
Nils Hollenstein <144909884+nilshollenstein@users.noreply.github.com>
2023-11-24 10:04:44 +0100

    commit before experimenting with flex

--- analogClock.js
+++ analogClock.js
@@ -1,16 +1,24 @@
 function updateClock() {
-  let date = new Date();
-  let hour = date.getHours();
-  let minute = date.getMinutes();
-  let second = date.getSeconds();
-  //document.write(`date${date} minute${minute} second${second}`);
-}
+  let now = new Date();
+  let hour = now.getHours() % 12; // 12-Stunden-Format
+  let minute = now.getMinutes();
+  let second = now.getSeconds();
 
-let date = updateClock()[0];
-let hour = updateClock()[1];
-let minute = updateClock()[2];
-let second = updateClock()[3];
+  let hourHand = document.querySelector(".hour-hand");
+  let minuteHand = document.querySelector(".min-hand");
+  let secondHand = document.querySelector(".second-hand");
+
+  let hourDeg = (360 / 12) * hour;
+  let minuteDeg = (360 / 60) * minute;
+  let secondDeg = (360 / 60) * second;
+
+  hourHand.style.transform = `rotate(${hourDeg}deg)`;
+  minuteHand.style.transform = `rotate(${minuteDeg}deg)`;
+  secondHand.style.transform = `rotate(${secondDeg}deg)`;
+  7;
+}
 
 setInterval(updateClock, 1000);
 
+// Initialer Aufruf, um die Uhrzeit sofort anzuzeigen
 updateClock();
