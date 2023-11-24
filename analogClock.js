62cf1ffa559ca00b96e9e0043ee1682d2fcbc1bb
Nils Hollenstein <144909884+nilshollenstein@users.noreply.github.com>
2023-11-24 09:17:39 +0100

    Commit before tests

--- analogClock.js
+++ analogClock.js
@@ -1,1 +1,16 @@
-document.write("Ello");
+function updateClock() {
+  let date = new Date();
+  let hour = date.getHours();
+  let minute = date.getMinutes();
+  let second = date.getSeconds();
+  //document.write(`date${date} minute${minute} second${second}`);
+}
+
+let date = updateClock()[0];
+let hour = updateClock()[1];
+let minute = updateClock()[2];
+let second = updateClock()[3];
+
+setInterval(updateClock, 1000);
+
+updateClock();
